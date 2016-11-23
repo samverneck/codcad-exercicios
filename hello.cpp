0ce77ba584ee1b78392326951efdfe9693d11c11
SAMUEL VERNECK <osamucaverneck@gmail.com>
2016-11-22 22:21:54 -0200

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // declaro a biblioteca aqui mano
+
+using namespace std;
+
+int main() { // declaro a função main e abro chaves bem aqui mano
+
+cout <<"Ola CodCad!\n"; // comando para imprimir uma frase na tela mano
+
+return 0; // informo ao nosso digníssimo sistema operacional que o meu programa terminou
+} // termino a main fechando as chaves mano
